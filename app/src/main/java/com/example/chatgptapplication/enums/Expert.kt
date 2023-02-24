d9a4ad761f31a6e79df1026d4e77ac8a4b6df31a
YamazakiYusuke <yusuke38098@gmail.com>
2023-02-24 22:07:42 +0900

    専門家の追加

--- app/src/main/java/com/example/chatgptapplication/enums/Expert.kt
+++ app/src/main/java/com/example/chatgptapplication/enums/Expert.kt
@@ -6,5 +6,11 @@ enum class Expert(val imageId: Int, val titleId: Int) {
     SCHOOL_TEACHER(R.drawable.school_teacher, R.string.school_teacher),
     LAWYER(R.drawable.lawyer, R.string.lawyer),
     SOFTWARE_ENGINEER(R.drawable.software_engineer, R.string.software_engineer),
-    DOCTOR(R.drawable.doctor, R.string.doctor)
+    DOCTOR(R.drawable.doctor, R.string.doctor),
+    PSYCHOLOGICAL_COUNSELOR(R.drawable.psychological_counselor, R.string.psychological_counselor),
+    ACCOUNTANT(R.drawable.accountant, R.string.accountant),
+    DENTIST(R.drawable.dentist, R.string.dentist),
+    ENGLISH_TEACHER(R.drawable.english_teacher, R.string.english_teacher),
+    DIETITIAN(R.drawable.dentist, R.string.dentist),
+    FARMER(R.drawable.farmer, R.string.farmer)
 }
