e808bf9b651d98a88be5ca439715721b7227eb14
YamazakiYusuke <yusuke38098@gmail.com>
2023-03-05 19:25:26 +0900

    アプリ紹介画面作成

--- app/src/main/java/com/example/myexpert/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/example/myexpert/fragments/IntroductionFragment.kt
@@ -1,26 +1,22 @@
 package com.example.myexpert.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.myexpert.R
+import com.example.myexpert.databinding.FragmentIntroductionBinding
 import com.example.myexpert.databinding.FragmentWelcomeBinding
 
-class WelcomeFragment : Fragment() {
-    private var _binding: FragmentWelcomeBinding? = null
+class IntroductionFragment : Fragment() {
+    private var _binding: FragmentIntroductionBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
-        binding.nextButton.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.frameLayout, IntroductionFragment()).commit()
-        }
+        _binding = FragmentIntroductionBinding.inflate(inflater, container, false)
         return binding.root
     }
 
